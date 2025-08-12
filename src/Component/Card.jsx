72a4082115b46514ff0c67e1bf8593c50c92740b
RJLerit19 <125624266+RJLerit19@users.noreply.github.com>
2025-08-11 21:34:56 -0700

    made it dynamic

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -6,14 +6,14 @@ const Card = (props) => {
       <div className="relative flex justify-center">
         <div className="absolute w-[80px] h-[80px] rounded-full border-2 border-primary top-5 z-index-10"></div>
       </div>
-      <div class="max-w-md h-[350px] p-6 mb-5 border rounded-2xl shadow-sm bg-gray-800 border-gray-700">
+      <div class="max-w-md xs:h-[full] sm:h-[350px] p-6 mb-5 border rounded-2xl shadow-sm bg-gray-800 border-gray-700">
         <div className="flex justify-center">{props.icon}</div>
         <a href="#">
           <h5 class="mb-2 pt-3 text-2xl text-center font-bold tracking-tight text-white">
             {props.title}
           </h5>
         </a>
-        <p class="mb-3 ml-6 font-normal text-gray-300">{props.description}</p>
+        <p class="mb-3 ml-4 font-normal text-gray-300">{props.description}</p>
       </div>
     </div>
   );
